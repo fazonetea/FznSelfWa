476638de4be1bb3b2cda525ee9ba5918d562f8f4
adulalhy <46670590+adulalhy@users.noreply.github.com>
2021-07-06 19:14:53 +0800

    BETA

---
+++ plugins/fzn_nopref.js
@@ -0,0 +1,11 @@
+(async () => {
+			switch(nopre){
+        case 'assalamualaikum':
+          reply('Waalaikumsalam')
+          break
+        case 'p':
+          reply('Salam Aja om')
+          break
+  			}
+			
+		})();	

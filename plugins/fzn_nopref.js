98138a1acd10957b1b60211a59808ce5a5950559
Adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-07-16 22:48:30 +0800

    Update fzn_nopref.js

--- plugins/fzn_nopref.js
+++ plugins/fzn_nopref.js
@@ -2,9 +2,11 @@
 			switch(nopre){
 				
         case 'assalamualaikum':
+        case 'Assalamualaikum':
           fzn.sendMessage(from, 'Waalaikumsalam', text, { quoted : msg })
           break
         case 'p':
+        case 'P':
           reply('Salam Aja om')
           break
   			}
